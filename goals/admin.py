cdeb817dd1c6b70c49743ecd7a385e4f27e5487d
Nikita <Nixon1111110@gmail.com>
2022-12-03 20:17:43 +0500

    add admin for comment, goal

--- goals/admin.py
+++ goals/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from goals.models.goal_category import GoalCategory
+from goals.models import GoalCategory, Goal, GoalComment
 
 
 @admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user__username")
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user__username")
+
+
+@admin.register(GoalComment)
+class GoalCommentAdmin(admin.ModelAdmin):
+    list_display = ("text", "user", "goal", "created", "updated")
     search_fields = ("title", "user__username")
