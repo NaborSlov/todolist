f47696b3d51f10566b2ea102f9bc50b9785f3eff
Nikita <Nixon1111110@gmail.com>
2022-12-12 20:13:35 +0500

    add create tg_user in bd

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.management import BaseCommand
 
+from bot.models import TgUser
 from bot.tg.client import TgClient
 
 
@@ -15,6 +16,13 @@ class Command(BaseCommand):
             res = tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
+                user_tg = TgUser.objects.filter(user_ud=item.message.from_.id).exists()
+
+                if not user_tg:
+                    TgUser.objects.create(user_ud=item.message.from_.id, chat_id=item.message.chat.id)
+                    tg_client.send_message(chat_id=item.message.chat.id, text="Привет новый пользователь")
+                else:
+                    tg_client.send_message(chat_id=item.message.chat.id, text="Привет старый пользователь")
 
     def handle(self, *args, **options):
         self.get_message_from_tg_bot()
